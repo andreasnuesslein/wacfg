d65734df7d9db0d854c4237bce1a9942f0093b13
Andreas Nüßlein <nutz@unfoog.de>
2010-06-22 15:35:30 +0200

    test-apps: added src_unpack def..

--- apps/wordpress/2.9.2/wacfg.py
+++ apps/wordpress/2.9.2/wacfg.py
@@ -3,5 +3,8 @@ from WaCfg import main, WaCfg, tools
 class WordPressHandler(WaCfg):
     __version__ = 0.1
 
+#def src_unpack(self):
+#    tools.archive_unpack('wordpress-2.9.2')
+
 main(WordPressHandler,
         source = "wordpress-2.9.2")
